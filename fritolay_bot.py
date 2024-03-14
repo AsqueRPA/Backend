69471cabe6975849513389a6428522cea4413bcb
Hugo Zhan <hugozhan0802@gmail.com>
2024-03-13 22:09:38 -0700

    more direct prompt

--- fritolay_bot.py
+++ fritolay_bot.py
@@ -24,8 +24,7 @@ if the full name is Cheetos Crunchy - Cheddar Jalapeno - 3.25 oz, you can try th
 - Cheddar Jalapeno
 - Cheetos
 - MAKE SURE TO ALSO TRY OTHER COMBINATION OF THE WORDS OR JUST THE BRAND NAME AS WELL
-DON'T include the size when you are searching, the ID for the search bar is usually close to 15 defintely NOT 215
-the search bar is near the top of the page, do not mistaken it for some input field in the item cards.
+DON'T include the size when you are searching, the ID for the search bar is usually 15 defintely NOT 215 and NOT 34 NOR 35.
 """
     while True:
         try:
